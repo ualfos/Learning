76f39f438c0166e1b8dfc54325b0a1730cbd47be
adrian <adrian.soflau@dohu.ro>
2020-10-21 18:12:48 +0300

    styling shop page

---
+++ Learning/src/app/shop/shop.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import { ProductService } from '../products/product.service';
+
+
+@Component({
+   
+    templateUrl: './shop.component.html',
+    styleUrls: ['./shop.component.scss']
+
+})
+export class ShopComponent{
+    constructor(){}
+}
